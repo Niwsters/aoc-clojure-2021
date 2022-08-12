1ea404a5952c305b991288b8634ce2de9348bd02
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2022-08-12 23:50:49 +0200

    Solved day 10 part

--- src/day10/solution.clj
+++ src/day10/solution.clj
@@ -57,7 +57,7 @@
             (if (= c (expected-closed result))
               (remove-last-open result)
               (unexpected-closed result c)))))
-      {:status      :ok
+      {:status      :incomplete
        :opens-queue []
        :index       0}
       characters)))
@@ -68,15 +68,57 @@
    \} 1197
    \> 25137} c))
 
+(defn- part1 [input]
+  (->> (lines input)
+       (map validate)
+       (filter #(not (= (:status %) :incomplete)))
+       (map #(:got %))
+       (map score)
+       (reduce +)))
+
+(defn- incomplete? [result]
+  (= (:status result) :incomplete))
+
+(defn- complete [result]
+  (map #(get pairs %) (reverse (:opens-queue result))))
+
+(defn- autocomp-value [c]
+  (get
+    {\) 1
+     \] 2
+     \} 3
+     \> 4}
+    c))
+
+(defn- score-autocomplete [characters]
+  (reduce
+    (fn [score c]
+      (+ (* score 5) (autocomp-value c)))
+    0
+    characters))
+
+(defn- pick-autocomplete-score [scores]
+  (println (int (Math/floor (/ (count scores) 2))))
+  (nth (sort scores) (int (Math/floor (/ (count scores) 2)))))
+
+(defn- part2 [input]
+  (->> (lines input)
+       (map validate)
+       (filter incomplete?)
+       (map complete)
+       (map score-autocomplete)
+       (sort)
+       (pick-autocomplete-score)))
+
 (comment
   pairs
 
-  (->> (lines (day10.input/input))
-       (map validate)
-       (filter #(not (= (:status %) :ok)))
-       (map #(:got %))
-       (map score)
-       (reduce +))
+  (part2 (day10.input/input))
+
+  (->> (validate "[({(<(())[]>[[{[]{<()<>>")
+       (complete)
+       (score-autocomplete))
+
   (validate "[(()[<>])]({[<{<<[]>>(")
   (validate "{([(<{}[<>[]}>{[]{[(<()>")
 )
