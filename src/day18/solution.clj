a4dbf10ef71a0cc38ea98a5dfd6683d3e6a62bba
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2022-09-03 20:10:08 +0200

    Start day 18

---
+++ src/day18/solution.clj
@@ -0,0 +1,7 @@
+(ns day18.solution
+  (:require [clojure.string :as str]
+            [cheshire.core :as cheshire]))
+
+(comment
+  (cheshire/parse-string "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]")
+)
